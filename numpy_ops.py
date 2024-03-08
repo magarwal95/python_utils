19b83711aa40e65463089c49bf2f26ebe60caf23
magarwal95 <mohitatul1995@gmail.com>
2024-03-08 23:06:31 +0530

    Created basic numpy operations

---
+++ numpy_ops.py
@@ -0,0 +1,29 @@
+"""
+contains array operations
+"""
+import numpy as np
+
+def remove_odd_number_from_array(arr):
+    """
+    removes the element from the array
+    :param arr: numpy array
+    :return: array
+    """
+    # Incorrectly delete elements from the array
+    for i in range(len(arr)):
+        if arr[i] % 2 == 0:
+            arr = np.delete(arr, i)
+
+    return arr
+
+
+def sum_of_squares_of_even_numbers(arr):
+    """
+    :param arr: numpy array
+    :return:
+    """
+    result = 0
+    for num in arr:
+        if num % 2 == 0:
+            result += num ** 2
+    return result
